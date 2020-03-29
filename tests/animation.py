845985aa635354745a44fe0b35f468c24e09167b
meguko2002 <meguko2002@gmail.com>
2020-03-29 18:40:11 +0900

    2値化ですりぬけ対策　TODO 入口の周囲の壁を塞ぐ

--- tests/animation.py
+++ tests/animation.py
@@ -7,31 +7,31 @@ import pyastar
 from os.path import join
 
 image_path = '../mazes'
-# MAZE_FPATH = join ( image_path , '4777.png' )
-# MAZE_FPATH = join(image_path, 'maze_large.png')
-MAZE_FPATH = join(image_path, 'mitsuoka_rockstar_01i.png')
+image_files=['4777.png' ,'maze_small.png','images.jpeg', 'rectangle_large.png']
+MAZE_FPATH = join ( image_path ,image_files[3])
 
 start = np.zeros ( 2 )  # [y, x]
-end = np.zeros ( 2 )
+goal = np.zeros ( 2 )
 
-select_point = 0
 THICK = 1
-mode = {'startset': 0 , 'endset': 0 , 'ready': 0}
+mode = {'startset': 0, 'endset': 0, 'ready': 0}
 
 
-def root_finder(maze , start , end):
+def root_finder(maze , start , goal):
     grid = cv2.cvtColor ( maze , cv2.COLOR_BGR2GRAY ).astype ( np.float32 )
+
+    _ , grid = cv2.threshold (grid, 127 , 255 , cv2.THRESH_BINARY )
     grid[grid == 0] = np.inf
     grid[grid == 255] = 1
 
     assert grid.min () == 1 , 'cost of moving must be at least 1'
     # ルート探索
-    root = pyastar.astar_path ( grid , start , end , allow_diagonal=False )
+    root = pyastar.astar_path ( grid , start , goal , allow_diagonal=False )
     return root
 
 
 def draw_circle(event , x , y , flags , param):
-    global start , end , maze , mode
+    global start , goal , maze , mode
     if mode['ready'] == 0:
         if event == cv2.EVENT_LBUTTONDOWN:
             if mode['startset'] == 0:
@@ -42,7 +42,7 @@ def draw_circle(event , x , y , flags , param):
                 mode['startset'] = 1
             elif mode['endset'] == 0:
                 print ( "goal set" )
-                end = np.array ( [y , x] )
+                goal = np.array ( [y , x] )
                 cv2.circle ( maze , (x , y) , 5 , (0 , 0 , 255) , 1 )
                 mode['endset'] = 1
 
@@ -65,22 +65,22 @@ if maze is None:
     exit ()
 
 cv2.namedWindow ( "Loaded image" )
-cv2.setMouseCallback ( "Loaded image" , draw_circle )
+cv2.setMouseCallback ( "Loaded image" , draw_circle)
 
 while True:
     cv2.imshow ( "Loaded image" , maze )
     key = cv2.waitKey ( 1 )
     # if key == #todo keyで中断
     if mode['startset'] == 1 and mode['endset'] == 1:
-        root = root_finder ( maze , start , end )  # ルート探索
+        root = root_finder ( original_image, start , goal )  # ルート探索
 
         if len ( root ) == 0:
             print ( "no root found" )
             mode['endset'] = 0
 
         else:
-            x = end[1]
-            y = end[0]
+            x = goal[1]
+            y = goal[0]
             cv2.circle ( maze , (x , y) , 5 , (0 , 0 , 255) , -1 )
             print ( "trace start" )
             mode['ready'] = 1
